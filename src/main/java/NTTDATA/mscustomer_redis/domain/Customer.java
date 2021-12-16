7da66caa8c4fe19379d704f1b5a5d5dfa3d18872
Dtravezano <Davidu.0622@gmail.com>
2021-12-16 13:20:29 -0500

    Add serializable

--- src/main/java/NTTDATA/mscustomer_redis/domain/Customer.java
+++ src/main/java/NTTDATA/mscustomer_redis/domain/Customer.java
@@ -5,11 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Customer {
+
+public class Customer implements Serializable{
     private String firstname;
     private String lastname;
     private String emailAddress;
